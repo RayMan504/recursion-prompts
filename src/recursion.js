fa096af9e83b1b5977d66a192a42f6141108bb1b
RayMan504 <uraeusmillet@gmail.com>
2019-04-08 09:09:11 -0500

    "(recursion) pseudocode for recursive sum"

--- src/recursion.js
+++ src/recursion.js
@@ -22,6 +22,9 @@ var factorial = function(n) {
 // 2. Compute the sum of an array of integers.
 // Example:  sum([1, 2, 3, 4, 5, 6]);  // 21
 var sum = function(array) {
+  //if array length is 0
+    //return 0
+  //add first element in array with recursive call on sliced array
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
